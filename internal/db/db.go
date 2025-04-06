035b34fa829a861f61c5dae273b3b1f2febf1b69
Kirill-PM <vlaspm21@gmail.com>
2025-04-06 13:11:59 +0300

    add user start

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,9 +2,11 @@ package db
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -43,5 +45,38 @@ func Init() error {
 		return fmt.Errorf("failed to migrate database: %w", err)
 	}
 
+	user, err := AddUser("1", "1@1.1", "123123123", "1", "1")
+	if err != nil {
+		// Обработка ошибки
+		log.Printf("Failed to add user: %v", err)
+	} else {
+		log.Printf("Added user with ID: %d", user.ID)
+	}
+
 	return nil
+}
+
+func AddUser(name, email, password, goal, gender string) (*User, error) {
+	// Хеширование пароля
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	// Создание пользователя
+	user := User{
+		Name:         name,
+		Email:        email,
+		PasswordHash: string(hashedPassword),
+		Goal:         goal,
+		Gender:       gender,
+	}
+
+	// Сохранение в базе данных
+	result := DB.Create(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &user, nil
 }
